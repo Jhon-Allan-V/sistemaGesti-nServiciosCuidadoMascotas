03836e231549b2c4ada0de3bd06a9dec3481614a
osses <osses@192.168.1.116>
2026-09-03 22:04:15 -0400

    Test Prueba

--- src/cl/pucv/mascotas/Main.java
+++ src/cl/pucv/mascotas/Main.java
@@ -6,6 +6,14 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("java.12131"));
+        
+        System.out.println(System.getProperty("java.12"));
+
+        System.out.println(System.getProperty("java.124124"));
+
+        System.out.println(System.getProperty("java.24142414"));
+
 
     }
     
