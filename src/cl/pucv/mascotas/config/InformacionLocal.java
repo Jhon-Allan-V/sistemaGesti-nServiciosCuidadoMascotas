33fa2da47ca557b4e36db406e29035227bc947d2
Jhon-Allan-V <jhon.veliz.h@mail.pucv.cl>
2026-09-05 01:55:14 -0400

    Cambio en nombre de atributo: abierto -> correo

--- src/cl/pucv/mascotas/config/InformacionLocal.java
+++ src/cl/pucv/mascotas/config/InformacionLocal.java
@@ -5,22 +5,22 @@ public class InformacionLocal {
     private String nombre;
     private String direccion;
     private String telefono;
-    private boolean abierto;
+    private String correo;
 
-    public InformacionLocal(String nombre, String direccion, String telefono, boolean abierto){
+    public InformacionLocal(String nombre, String direccion, String telefono, String correo){
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
-        this.abierto = abierto;
+        this.correo = correo;
     }
 
     public String getNombre(){return nombre;}
     public String getDireccion(){return direccion;}
     public String getTelefono(){return telefono;}
-    public boolean getAbierto(){return abierto;}
+    public String getCorreo(){return correo;}
 
     public void setNombre(String nombre){this.nombre = nombre;}
     public void setDireccion(String direccion){this.direccion = direccion;}
     public void setTelefono(String telefono){this.telefono = telefono;}
-    public void setAbierto(boolean abierto){this.abierto = abierto;}
+    public void setCorreo(String correo){this.correo = correo;}
 }
