360439c6f4f9b26634f31c4e436aa984da1d3c24
Jhon-Allan-V <jhon.veliz.h@mail.pucv.cl>
2026-09-05 01:56:16 -0400

    Se arranca la aplicacion

--- src/cl/pucv/mascotas/Main.java
+++ src/cl/pucv/mascotas/Main.java
@@ -5,15 +5,8 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.println(System.getProperty("java.version"));
-        System.out.println(System.getProperty("java.12131"));
-        
-        System.out.println(System.getProperty("java.12"));
-
-        System.out.println(System.getProperty("java.124124"));
-
-        System.out.println(System.getProperty("java.24142414"));
-
+        Aplicacion app = new Aplicacion();
+        app.arrancar();
 
     }
     
