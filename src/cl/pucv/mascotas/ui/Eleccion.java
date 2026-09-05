484d9b3d3785f8323fcd3c4c68c98b01b96559e3
Jhon-Allan-V <jhon.veliz.h@mail.pucv.cl>
2026-09-05 01:58:12 -0400

    Se implementa: atributos + constructor + metodo: tomar eleccion

--- src/cl/pucv/mascotas/ui/Eleccion.java
+++ src/cl/pucv/mascotas/ui/Eleccion.java
@@ -1,5 +1,42 @@
 package cl.pucv.mascotas.ui;
 
+import cl.pucv.mascotas.facade.SistemaFacade;
+//import cl.pucv.mascotas.ui.*;
+import java.util.Scanner;
+
+
 public class Eleccion {
     
+    private InterfazUsuario interfaz; 
+    private final SistemaFacade sistema; 
+
+    public Eleccion(SistemaFacade sistema){
+          this.sistema = sistema;
+    }
+
+    public void tomarEleccion(){
+
+        Scanner eleccion = new Scanner(System.in);
+
+        System.out.println("Seleccione el modo de ejecucion de el programa:");
+        System.out.println("1. Modo Consola o Terminal.");
+        System.out.println("2. Modo Ventana (interfaz).");
+        System.out.println("Ingrese opcion (1 o 2): ");
+
+        int opcion = 1;
+
+        try{
+            opcion = Integer.parseInt(eleccion.nextLine());
+        } catch(NumberFormatException e){
+            System.out.println("Entrada invalida; Se iniciara en modo Consola o Terminal por defecto.");
+        }
+
+        if (opcion == 2){
+            this.interfaz = new PantallaInterfaz(this.sistema);
+        } else {
+            this.interfaz = new ConsolaTerminal(this.sistema);
+        }
+        
+        this.interfaz.iniciar();
+    }
 }
