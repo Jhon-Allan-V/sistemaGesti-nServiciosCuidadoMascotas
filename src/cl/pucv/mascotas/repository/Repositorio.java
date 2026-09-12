5cb29dc39727fc8a7b1a8d15df962e7951477c0b
osses <osses@192.168.1.116>
2026-09-11 23:49:32 -0300

    Repositorios de java

--- src/cl/pucv/mascotas/repository/Repositorio.java
+++ src/cl/pucv/mascotas/repository/Repositorio.java
@@ -1,5 +1,15 @@
 package cl.pucv.mascotas.repository;
 
-public class Repositorio {
-    
+import cl.pucv.mascotas.exception.PersistenciaException;
+import java.util.List;
+
+public interface Repositorio<T, ID> {
+
+    void guardar(T entidad) throws PersistenciaException;
+
+    void eliminar(ID id) throws PersistenciaException;
+
+    T buscarPorId(ID id) throws PersistenciaException;
+
+    List<T> listarTodos() throws PersistenciaException;
 }
