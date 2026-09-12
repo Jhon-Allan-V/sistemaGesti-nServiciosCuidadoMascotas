0aa4db4c6785bc2f4075a967731f20f3f8e8a184
osses <osses@192.168.1.116>
2026-09-11 23:51:26 -0300

    Se añaden setter y gettes en veterinaria

--- src/cl/pucv/mascotas/model/Veterinaria.java
+++ src/cl/pucv/mascotas/model/Veterinaria.java
@@ -2,27 +2,54 @@ package cl.pucv.mascotas.model;
 
 import java.time.LocalDate;
 
-public class Veterinaria extends Servicio{
+public class Veterinaria extends Servicio {
 
     private String nombreVeterinario;
     private String especialidad;
     private String licencia;
 
-    public Veterinaria(int codigo, String tipo, String descripcion, 
-        double costo, LocalDate fecha, String nombreVeterinario, 
-        String especialidad, String licencia){
+    public Veterinaria(int codigo, String tipo, String descripcion,
+            double costo, LocalDate fecha, String nombreVeterinario,
+            String especialidad, String licencia) {
 
         super(codigo, tipo, descripcion, costo, fecha);
         this.nombreVeterinario = nombreVeterinario;
         this.especialidad = especialidad;
         this.licencia = licencia;
     }
-    
+
     @Override
-    public float calcularPrecioServicio(){
+    public float calcularPrecioServicio() {
         return 1.2f;
     }
 
-    public void atenderMascota(){}
-    public void emitirDiagnostico(){}
+    public void atenderMascota() {
+    }
+
+    public void emitirDiagnostico() {
+    }
+
+    public String getNombreVeterinario() {
+        return nombreVeterinario;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setNombreVeterinario(String nombreVeterinario) {
+        this.nombreVeterinario = nombreVeterinario;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
 }
